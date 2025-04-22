609424fe43a5b4698cfd9393a348a6745e5fd960
sgmt <s.shigematsu17@gmail.com>
2025-04-22 13:40:44 +0900

    ログインボタンのタイプをsubmitからbuttonに変更し、不要なコードを削除

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,29 +4,20 @@ const userInputPassword = document.getElementById("password");
 
 // // 初期表示
 window.addEventListener("load", () => {
-    
+    if (localStorage.getItem("userEmail")) {
+        userInputEmail.value = localStorage.getItem("userEmail");
+    }
+    if (localStorage.getItem("userPassword")) {
+        userInputPassword.value = localStorage.getItem("userPassword");
+    }
 });
-// if (localStorage.getItem("userId")) {
-//     userInputEmail.value = localStorage.getItem("userId");
-// }
-// // ログインボタンが押されたときの処理
 
-// // Login押されたとき
-// // 入力内容をローカルストレージに保存
-// document.getElementById("login").addEventListener("click", function() {
-//     const userId = document.getElementById("userId").value;
-//     const password = document.getElementById("password").value;
-//     const userData = {
-//         userId: userId,
-//         password: password
-//     };
-//     localStorage.setItem("userData", JSON.stringify(userData));
-//     alert("ログインしました。");
-// });
-
-
+// ログインボタンがクリックされたときの処理
 loginBtn.addEventListener("click", () => {
     let userEmail = userInputEmail.value;
     let userPassword = userInputPassword.value;
     console.log(userEmail,userPassword)
+
+    localStorage.setItem('userEmail', userEmail);
+    localStorage.setItem('userPassword', userPassword);
 });
